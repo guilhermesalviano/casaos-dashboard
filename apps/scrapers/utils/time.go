b11cb64e283545196ea7ead67920e76240b5e5b2
Guilherme <guilherme.salviano12@outlook.com>
2026-03-04 16:26:00 -0300

    refactor(scrapers): add submodule

--- apps/scrapers/utils/time.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-func TimeOnly(dt string) string {
-	// dt format: "2006-01-02 15:04"
-	if len(dt) >= 16 {
-		return dt[11:16]
-	}
-	return dt
-}
--- apps/scrapers/utils/truncate.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-func Truncate(s string, n int) string {
-	if len(s) <= n {
-		return s
-	}
-	return s[:n-1] + "…"
-}
